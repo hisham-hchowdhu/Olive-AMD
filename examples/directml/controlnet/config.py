2476e227e390cb8a8c2c147b13f842371692013b
Yazdanbakhsh <oyazdanb@amd.com>
2024-02-13 15:12:23 -0500

    adding controlnet as directml example for Olive

---
+++ examples/directml/controlnet/config.py
@@ -0,0 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+vae_sample_size = 512
+unet_sample_size = 64
+cross_attention_dim = 768
+unet_in_channels=4
+num_tokens=77
